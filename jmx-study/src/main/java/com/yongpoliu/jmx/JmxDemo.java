67f64310a0d86bcee0e0877af87ce65501224c96
danny_liu <liuyongpo@gmail.com>
2014-04-12 23:12:11 +0800

    加上一些日志依赖，演示下jdk提供的系统以及jvm信息的jmx接口，这样以后可以使用提供的MXBean进行监控

--- jmx-study/src/main/java/com/yongpoliu/jmx/JmxDemo.java
+++ jmx-study/src/main/java/com/yongpoliu/jmx/JmxDemo.java
@@ -1,9 +1,14 @@
 package com.yongpoliu.jmx;
 
 import com.sun.jdmk.comm.HtmlAdaptorServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.management.*;
 import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryUsage;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.ThreadInfo;
 
 /**
  * native JMX demo
@@ -12,7 +17,9 @@ import java.lang.management.ManagementFactory;
  */
 public class JmxDemo {
 
-    public static void main(String[] args) throws MalformedObjectNameException, NotCompliantMBeanException, InstanceAlreadyExistsException, MBeanRegistrationException {
+    private final static Logger LOG = LoggerFactory.getLogger(JmxDemo.class);
+
+    public static void main(String[] args) throws Exception {
         MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
 
         platformMBeanServer.registerMBean(new Config(), new ObjectName("jmx-demo:name=config"));
@@ -23,5 +30,24 @@ public class JmxDemo {
         platformMBeanServer.registerMBean(htmlAdaptorServer, new ObjectName("jmx-demo:name=htmlAdaptor,port=8091"));
 
         htmlAdaptorServer.start();
+
+        LOG.warn("jmx-demo started......");
+
+        OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+
+        double systemLoadAverage = operatingSystemMXBean.getSystemLoadAverage();
+
+        LOG.warn("Current system load is {}", systemLoadAverage);
+
+        ThreadInfo[] allThreads = ManagementFactory.getThreadMXBean().dumpAllThreads(true, true);
+
+        for (ThreadInfo threadInfo : allThreads) {
+            LOG.warn(threadInfo.toString());
+        }
+
+        MemoryUsage heapMemoryUsage = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
+
+        LOG.warn("heap used is {}", heapMemoryUsage);
+
     }
 }
